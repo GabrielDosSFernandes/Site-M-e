d3ef028dba4bcd2a3b6d4fa32e28b2516dfd7947
GabrielDosSFernandes <145733885+GabrielDosSFernandes@users.noreply.github.com>
2024-09-17 20:31:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const botao = document.getElementById('meuBotao');
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener('click', function() {
+    alert('Você clicou no botão!');
+});
